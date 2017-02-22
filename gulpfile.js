bed8f1280ef3bc44ffbae1f7120d302f4d5fa557
Takashi Kitajima <inc@2inc.org>
2017-02-22 09:29:08 +0900

    Some fixes

--- gulpfile.js
+++ gulpfile.js
@@ -101,7 +101,7 @@ gulp.task('ejs', function() {
     //.pipe(plumber())
     .pipe(ejs(
       {
-        version: '6.0.2',
+        version: '6.1.1',
         css    : '/assets/css',
         js     : '/assets/js',
         images : '/assets/images',
