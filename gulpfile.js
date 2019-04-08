d03c8b49a0227c96cb668c7ef109a97636f7a806
Takashi Kitajima <inc@2inc.org>
2019-04-08 13:38:50 +0900

    9.2.11

--- gulpfile.js
+++ gulpfile.js
@@ -115,7 +115,7 @@ gulp.task('ejs', function() {
     //.pipe(plumber())
     .pipe(ejs(
       {
-        version: '9.2.10',
+        version: '9.2.11',
         css    : '/assets/css',
         js     : '/assets/js',
         images : '/assets/images'
